5e6923769e11d25c34a080f68fbda6eb726ce243
fran2 <fran2@PC-FRANCO>
2025-05-11 00:49:25 -0300

    ....

--- Listo_TPIPWA2025/Models/Usuario.cs
+++ Listo_TPIPWA2025/Models/Usuario.cs
@@ -2,5 +2,25 @@
 {
     public class Usuario
     {
+        public Usuario() { }
+
+        public Usuario(Usuario pUsuario)
+        {
+            Id = pUsuario.Id;
+            Nombre = pUsuario.Nombre;
+            Apellido = pUsuario.Apellido;
+            Email = pUsuario.Email;
+        }
+
+        public Usuario(int pId, string pNom, string pA, string pE, string pwd) { Id = pId; Nombre = pNom; Apellido = pA; Email = pE; Password = pwd; }
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
     }
 }
